7a48f23766f7b643ddba4e23f8dfd5ed36e7e6cf
soosap <prasath.soosaithasan@gmail.com>
2016-12-26 15:36:06 +0100

    Add action creator to handle logging out a user

--- src/actions/users/logoutUser.action.js
+++ src/actions/users/logoutUser.action.js
@@ -1,5 +1,13 @@
+import { browserHistory } from 'react-router';
 import { UNAUTH_USER } from 'actions/types';
 
 export function logoutUser() {
+  // Redirect user to home screen
+  browserHistory.push('/');
+
+  // Remove token from localStorage
+  localStorage.removeItem('token');
+
+  // Set authenticated flag to false
   return { type: UNAUTH_USER };
 }
