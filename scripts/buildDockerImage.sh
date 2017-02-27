c4e2730dbc408aaed97c02556f479f5b299afea2
soosap <prasath.soosaithasan@gmail.com>
2017-02-27 13:19:04 +0100

    Improve Docker build process

--- scripts/buildDockerImage.sh
+++ scripts/buildDockerImage.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t ${DOCKER_HUB_ORGANIZATION}/${APP_NAME}:${APP_VERSION} .
+docker build -f docker/build/Dockerfile -t ${DOCKER_HUB_USERNAME}/${APP_NAME}:${APP_VERSION} .
--- scripts/publishDockerImage.sh
+++ scripts/publishDockerImage.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker login -u ${DOCKER_HUB_USERNAME} -p ${DOCKER_HUB_PASSWORD}
-docker push ${DOCKER_HUB_ORGANIZATION}/${APP_NAME}:${APP_VERSION}
+docker push ${DOCKER_HUB_USERNAME}/${APP_NAME}:${APP_VERSION}
