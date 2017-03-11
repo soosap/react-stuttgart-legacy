b68b065b2400c5d2fed8b5b29d5458c9f7a2b9f1
SquashPaT <pm@webmix-it.de>
2017-03-11 22:22:28 +0100

    refactor footer swap out <Links/>

--- src/components/common/Footer/index.jsx
+++ src/components/common/Footer/index.jsx
@@ -1,16 +1,11 @@
 /* @flow */
 import React from 'react';
 import styled from 'styled-components';
-import { compose, withHandlers } from 'recompose';
 
-import { colors, media } from '../../../assets/styles';
-import { BECOME_SPEAKER } from '../../modals/types';
+import { colors } from '../../../assets/styles';
 import Team from './Team';
 import SocialBar from './SocialBar';
-
-type Props = {
-  showModal: () => void,
-};
+import Links from './Links';
 
 const Wrapper = styled.div`
   background: rgba(${colors.secondaryDarkRGB}, 0.9);
@@ -22,75 +17,24 @@ const Wallpaper = styled.div`
   background-size: 100%, 100%;
 `;
 
-const Links = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  padding: 1rem;
-
-  ${media.tabletAndLargerThanThat} {
-    justify-content: space-between;
-  }
-`;
-
-const Link = styled.a`
-  margin: 5px;
-  color: ${colors.white};
-  &:hover {
-    color: ${colors.secondary};
-    text-decoration: underline;
-  }
-`;
-
-const Brand = styled.span`
-  margin: 5px;
-  color: ${colors.secondary};
-`;
-
-const Left = styled.div`
-  display: flex;
-`;
-
-const Right = styled.div`
-  display: flex;
-`;
-
 const Divider = styled.h3`
   color: white;
   font-family: Lullabies-Text;
 `;
 
-const enhance = compose(
-  withHandlers({
-    onClick: (props: Props) => {
-      props.showModal(BECOME_SPEAKER);
-    },
-  }),
-);
-
-const Footer = enhance(({ onClick }) => (
-  <Wallpaper>
-    <Wrapper>
-      <SocialBar/>
-      <div className="ui horizontal inverted divider">
-        <Divider>Organized by</Divider>
-      </div>
-      <Team />
-      <Links>
-        <Left>
-          <Brand className="disabled item">© #ReactStuttgart</Brand>
-          <Link className="item" href="#">Terms</Link>
-          <Link className="item" href="#">Contact Us</Link>
-        </Left>
-        <Right>
-          <Link className="item" href="#">About Us</Link>
-          <Link className="item" onClick={onClick}>
-            Become a Speaker
-          </Link>
-        </Right>
-      </Links>
-    </Wrapper>
-  </Wallpaper>
-));
+const Footer = () => {
+  return (
+    <Wallpaper>
+      <Wrapper>
+        <SocialBar/>
+        <div className="ui horizontal inverted divider">
+          <Divider>Organized by</Divider>
+        </div>
+        <Team />
+        <Links/>
+      </Wrapper>
+    </Wallpaper>
+  );
+};
 
 export default Footer;
