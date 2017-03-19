9bd65f30c95239a113e591bc48e5ea5b5a16f470
soosap <prasath.soosaithasan@gmail.com>
2017-03-19 22:08:32 +0100

    Add copy-webpack-plugin to provide logo in dist root

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ import BrowserSyncPlugin from 'browser-sync-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import WebpackMd5HashPlugin from 'webpack-md5-hash';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
 
 export default function ({ development }) {
   /*
@@ -21,21 +22,21 @@ export default function ({ development }) {
 
   // Webpack => HTML | Variables needed to render index.html
   // used by HtmlWebpackPlugin
-  const buildTimeVarsHtml = development ? {
-    NODE_ENV: process.env.NODE_ENV,
-  } : {
-    NODE_ENV: process.env.NODE_ENV,
-    FACEBOOK_APP_ID: process.env.FACEBOOK_APP_ID,
-    SEGMENT_WRITE_KEY: process.env.SEGMENT_WRITE_KEY,
-  };
+  const buildTimeVarsHtml = development
+    ? {
+      NODE_ENV: process.env.NODE_ENV,
+    }
+    : {
+      NODE_ENV: process.env.NODE_ENV,
+      FACEBOOK_APP_ID: process.env.FACEBOOK_APP_ID,
+      SEGMENT_WRITE_KEY: process.env.SEGMENT_WRITE_KEY,
+    };
 
   // Webpack => JS | Variables needed to build bundle.js
   // used by EnvironmentPlugin
-  const buildTimeVarsJs = development ? [
-    'NODE_ENV', 'APP_NAME',
-  ] : [
-    'NODE_ENV', 'APP_NAME',
-  ];
+  const buildTimeVarsJs = development
+    ? ['NODE_ENV', 'APP_NAME']
+    : ['NODE_ENV', 'APP_NAME'];
 
   return {
     resolve: {
@@ -47,96 +48,151 @@ export default function ({ development }) {
         store: path.join(__dirname, 'src', 'store'),
         images: path.join(__dirname, 'src', 'assets', 'images'),
         // vendor libs
-        react: path.join(__dirname, 'node_modules', 'react', 'dist', development ? 'react.js' : 'react.min.js'),
-        'react-dom': path.join(__dirname, 'node_modules', 'react-dom', 'dist', development ? 'react-dom.js' : 'react-dom.min.js'),
-        'react-redux': path.join(__dirname, 'node_modules', 'react-redux', 'dist', development ? 'react-redux.js' : 'react-redux.min.js'),
-        'react-router-redux': path.join(__dirname, 'node_modules', 'react-router-redux', 'dist', development ? 'ReactRouterRedux.js' : 'ReactRouterRedux.min.js'),
-        redux: path.join(__dirname, 'node_modules', 'redux', 'dist', development ? 'redux.js' : 'redux.min.js'),
-        'redux-form': path.join(__dirname, 'node_modules', 'redux-form', 'dist', development ? 'redux-form.js' : 'redux-form.min.js'),
-        'styled-components': path.join(__dirname, 'node_modules', 'styled-components', 'lib', 'index.js'),
+        react: path.join(
+          __dirname,
+          'node_modules',
+          'react',
+          'dist',
+          development ? 'react.js' : 'react.min.js',
+        ),
+        'react-dom': path.join(
+          __dirname,
+          'node_modules',
+          'react-dom',
+          'dist',
+          development ? 'react-dom.js' : 'react-dom.min.js',
+        ),
+        'react-redux': path.join(
+          __dirname,
+          'node_modules',
+          'react-redux',
+          'dist',
+          development ? 'react-redux.js' : 'react-redux.min.js',
+        ),
+        'react-router-redux': path.join(
+          __dirname,
+          'node_modules',
+          'react-router-redux',
+          'dist',
+          development ? 'ReactRouterRedux.js' : 'ReactRouterRedux.min.js',
+        ),
+        redux: path.join(
+          __dirname,
+          'node_modules',
+          'redux',
+          'dist',
+          development ? 'redux.js' : 'redux.min.js',
+        ),
+        'redux-form': path.join(
+          __dirname,
+          'node_modules',
+          'redux-form',
+          'dist',
+          development ? 'redux-form.js' : 'redux-form.min.js',
+        ),
+        'styled-components': path.join(
+          __dirname,
+          'node_modules',
+          'styled-components',
+          'lib',
+          'index.js',
+        ),
       },
       extensions: ['.js', '.jsx', '.json'],
     },
     devtool: development ? 'inline-source-map' : 'source-map',
-    entry: development ? [
-      'regenerator-runtime/runtime',
-      // enables use of modern ESNext features like async/await and generator functions
-      'webpack-hot-middleware/client?reload=false',
-      // ?reload=false parameter tells webpack to avoid page reloads
-      path.resolve(__dirname, 'src/index'),
-    ] : {
-      vendor: path.resolve(__dirname, 'src/vendor'),
-      main: path.resolve(__dirname, 'src/index'),
-    },
+    entry: development
+      ? [
+        'regenerator-runtime/runtime',
+        // enables use of modern ESNext features like async/await and generator functions
+        'webpack-hot-middleware/client?reload=false',
+        // ?reload=false parameter tells webpack to avoid page reloads
+        path.resolve(__dirname, 'src/index'),
+      ] : {
+        vendor: path.resolve(__dirname, 'src/vendor'),
+        main: path.resolve(__dirname, 'src/index'),
+      },
     target: 'web',
-    output: development ? {
-      path: path.resolve(__dirname, 'src'),
-      publicPath: '/',
-      filename: 'bundle.js',
-    } : {
-      path: path.resolve(__dirname, 'dist'),
-      publicPath: '/',
-      filename: '[name].[chunkhash].js',
-    },
-    plugins: development ? [ // ====================================================================
-      new BrowserSyncPlugin({
-        host: 'localhost',
-        port: 9090,
-        proxy: 'http://localhost:8080/',
-        open: false,
-      }, { // plugin options
-        // Prevent BrowserSync from reloading the page
-        reload: false,
-      }),
-      // Generate HTML file that contains references to generated bundles
-      new HtmlWebpackPlugin({
-        template: 'src/index.ejs',
-        favicon: 'src/assets/images/favicon.ico',
-        inject: true,
-        ...buildTimeVarsHtml,
-      }),
-      // Make bundle.js build vars available via "process.env"
-      new webpack.EnvironmentPlugin(buildTimeVarsJs),
-      // Enable Hot Module Replacement in development
-      new webpack.HotModuleReplacementPlugin(),
-      // Keep errors from breaking our HMR experience
-      new webpack.NoErrorsPlugin(),
-      // Print more readable module names in the browser console on HMR updates
-      new webpack.NamedModulesPlugin(),
-    ] : [ // =======================================================================================
-      // Hash bundle-files using MD5 so that their names change when the content changes
-      new WebpackMd5HashPlugin(),
-      // Generate an external css file with a hash in the filename
-      new ExtractTextPlugin('[name].[contenthash].css'),
-      // Use CommonChunkPlugin to create a separate bundle of vendor libraries
-      // so that they are cached separately improving performance
-      new webpack.optimize.CommonsChunkPlugin({
-        names: ['vendor', 'manifest'],
-      }),
-      // Minify JS
-      new webpack.optimize.UglifyJsPlugin(),
-      // Generate HTML file that contains references to generated bundles
-      new HtmlWebpackPlugin({
-        template: 'src/index.ejs',
-        favicon: 'src/assets/images/favicon.ico',
-        minify: {
-          removeComments: true,
-          collapseWhitespace: true,
-          removeRedundantAttributes: true,
-          useShortDoctype: true,
-          removeEmptyAttributes: true,
-          removeStyleLinkTypeAttributes: true,
-          keepClosingSlash: true,
-          minifyJS: true,
-          minifyCSS: true,
-          minifyURLs: true,
-        },
-        inject: true,
-        ...buildTimeVarsHtml,
-      }),
-      // Make bundle.js build vars available via "process.env"
-      new webpack.EnvironmentPlugin(buildTimeVarsJs),
-    ], // ==========================================================================================
+    output: development
+      ? {
+        path: path.resolve(__dirname, 'src'),
+        publicPath: '/',
+        filename: 'bundle.js',
+      } : {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+        filename: '[name].[chunkhash].js',
+      },
+    plugins: development
+      ? [
+        // ====================================================================
+        new BrowserSyncPlugin(
+          {
+            host: 'localhost',
+            port: 9090,
+            proxy: 'http://localhost:8080/',
+            open: false,
+          },
+          {
+            // plugin options
+            // Prevent BrowserSync from reloading the page
+            reload: false,
+          },
+        ),
+        // Generate HTML file that contains references to generated bundles
+        new HtmlWebpackPlugin({
+          template: 'src/index.ejs',
+          favicon: 'src/assets/images/favicon.ico',
+          inject: true,
+          ...buildTimeVarsHtml,
+        }),
+        // Make bundle.js build vars available via "process.env"
+        new webpack.EnvironmentPlugin(buildTimeVarsJs),
+        // Enable Hot Module Replacement in development
+        new webpack.HotModuleReplacementPlugin(),
+        // Keep errors from breaking our HMR experience
+        new webpack.NoErrorsPlugin(),
+        // Print more readable module names in the browser console on HMR updates
+        new webpack.NamedModulesPlugin(),
+      ] : [
+        // =======================================================================================
+        // Hash bundle-files using MD5 so that their names change when the content changes
+        new WebpackMd5HashPlugin(),
+        // Generate an external css file with a hash in the filename
+        new ExtractTextPlugin('[name].[contenthash].css'),
+        // Use CommonChunkPlugin to create a separate bundle of vendor libraries
+        // so that they are cached separately improving performance
+        new webpack.optimize.CommonsChunkPlugin({
+          names: ['vendor', 'manifest'],
+        }),
+        // Minify JS
+        new webpack.optimize.UglifyJsPlugin(),
+        // Generate HTML file that contains references to generated bundles
+        new HtmlWebpackPlugin({
+          template: 'src/index.ejs',
+          favicon: 'src/assets/images/favicon.ico',
+          minify: {
+            removeComments: true,
+            collapseWhitespace: true,
+            removeRedundantAttributes: true,
+            useShortDoctype: true,
+            removeEmptyAttributes: true,
+            removeStyleLinkTypeAttributes: true,
+            keepClosingSlash: true,
+            minifyJS: true,
+            minifyCSS: true,
+            minifyURLs: true,
+          },
+          inject: true,
+          ...buildTimeVarsHtml,
+        }),
+        new CopyWebpackPlugin([
+          // {output}/logo.png => Facebook Open Graph requires this file.
+          { from: 'src/assets/images/reactstuttgart@1x.png', to: 'logo.png' },
+        ]),
+        // Make bundle.js build vars available via "process.env"
+        new webpack.EnvironmentPlugin(buildTimeVarsJs),
+      ], // ====================================================================
     module: {
       rules: [
         {
@@ -144,15 +200,16 @@ export default function ({ development }) {
           exclude: /node_modules/,
           use: ['babel-loader'],
         },
-        development ? {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
-        } : {
-          test: /\.css$/,
-          loader: ExtractTextPlugin.extract({
-            loader: 'css-loader',
-          }),
-        },
+        development
+          ? {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader'],
+          } : {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract({
+              loader: 'css-loader',
+            }),
+          },
         {
           test: /\.json$/,
           loader: 'json-loader',
@@ -169,19 +226,20 @@ export default function ({ development }) {
           test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
           loader: 'url-loader?limit=10000&mimetype=application/octet-stream',
         },
-        development ? {
-          test: /\.(jpe?g|png|gif|svg)$/,
-          use: [{ loader: 'url-loader', options: { limit: 40000 } }],
-        } : {
-          test: /\.(jpe?g|png|gif|svg)$/,
-          use: [
-            {
-              loader: 'url-loader',
-              options: { limit: 40000 },
-            },
-            'image-webpack-loader',
-          ],
-        },
+        development
+          ? {
+            test: /\.(jpe?g|png|gif|svg)$/,
+            use: [{ loader: 'url-loader', options: { limit: 40000 } }],
+          } : {
+            test: /\.(jpe?g|png|gif|svg)$/,
+            use: [
+              {
+                loader: 'url-loader',
+                options: { limit: 40000 },
+              },
+              'image-webpack-loader',
+            ],
+          },
       ],
     },
   };
