3942f347cb6f624916786903ce7677e1034d226f
soosap <prasath.soosaithasan@gmail.com>
2017-05-18 14:08:08 +0200

    Add message field to BecomeSpeaker form

--- src/components/forms/BecomeSpeaker/BecomeSpeakerForm.js
+++ src/components/forms/BecomeSpeaker/BecomeSpeakerForm.js
@@ -9,11 +9,19 @@ type Props = {
   handleSubmit: () => void,
 };
 
+const submit = (values: Object) => {
+  console.log('values', values);
+};
+
 const BecomeSpeakerForm = ({ handleSubmit }: Props) => (
-  <form onSubmit={handleSubmit}>
+  <form onSubmit={handleSubmit(submit)}>
     <div>
       <label htmlFor="email">Email</label>
       <Field name="email" component="input" type="email" />
+    </div>
+    <div>
+      <label htmlFor="message">Message</label>
+      <Field name="message" component="input" />
     </div>
     <button type="submit">Submit</button>
   </form>
--- src/components/modals/BecomeSpeaker/BecomeSpeakerModal.js
+++ src/components/modals/BecomeSpeaker/BecomeSpeakerModal.js
@@ -1,18 +1,22 @@
 /* @flow */
-import React from 'react';
+import React, { Component } from 'react';
 import { Button } from '@saronia/saronia-ui';
 
 import Modal from '../templates/Default';
 import BecomeSpeakerForm from '../../forms/BecomeSpeaker';
 
-const BecomeSpeakerModal = () => (
-  <Modal>
-    Become a speaker!
-    Work in progress bro...
-    <Button accent>Subscribe</Button>
-    hello
-    <BecomeSpeakerForm />
-  </Modal>
-);
+class BecomeSpeakerModal extends Component {
+  render() {
+    return (
+      <Modal>
+        Become a speaker!
+        Work in progress bro...
+        <Button accent>Subscribe</Button>
+        hello
+        <BecomeSpeakerForm />
+      </Modal>
+    );
+  }
+}
 
 export default BecomeSpeakerModal;
